5a911f3d375463d532b761e1fd4564a00489d99c
Tom <tom.kemper@hu.nl>
2024-07-01 21:44:49 +0200

    Wow, een bug in de .gitignore, dat was gevaarlijk!

---
+++ src/main/java/nl/hu/bep2/casino/chips/data/JpaChipsRepository.java
@@ -0,0 +1,31 @@
+package nl.hu.bep2.casino.chips.data;
+
+import jakarta.persistence.EntityManager;
+import nl.hu.bep2.casino.chips.domain.Chips;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class JpaChipsRepository implements ChipsRepository {
+
+    private EntityManager entities;
+
+    public JpaChipsRepository(EntityManager entities) {
+        this.entities = entities;
+    }
+
+    @Override
+    public Optional<Chips> findByUsername(String username) {
+        Optional<Chips> result = entities.createQuery("select c from Chips c where c.username = ?1", Chips.class)
+                .setParameter(1, username)
+                .getResultList().stream().findFirst();
+
+        return result;
+    }
+
+    @Override
+    public void save(Chips chips) {
+        entities.persist(chips);
+    }
+}
